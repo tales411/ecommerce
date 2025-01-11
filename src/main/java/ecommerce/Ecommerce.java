fdd85ea3f10581c078930287f4dd38d3863a296f
tales411 <tales.tatatdc@gmail.com>
2025-01-11 07:49:12 -0300

    a

--- src/main/java/ecommerce/Ecommerce.java
+++ src/main/java/ecommerce/Ecommerce.java
@@ -3,6 +3,7 @@ package ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Ecommerce {
     public static void main(String[] args) {
